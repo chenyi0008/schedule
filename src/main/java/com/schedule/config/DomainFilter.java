ba89c68d12130b90f1f8fa25703f641b688b646b
chenyi <71563698@qq.com>
2023-01-11 19:29:00 +0800

    解决跨域问题

---
+++ src/main/java/com/schedule/config/DomainFilter.java
@@ -0,0 +1,41 @@
+package com.schedule.config;
+
+
+
+import org.springframework.context.annotation.Configuration;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * 解决跨域问题
+ */
+@Configuration
+public class DomainFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+
+        //修改请求头
+        HttpServletResponse response = (HttpServletResponse) res;
+        response.setHeader("Access-Control-Allow-Origin", "*");//ip地址
+        response.setHeader("Access-Control-Allow-Methods", "*");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.addHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, token, authorization");
+
+        chain.doFilter(req, res);
+
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
