e987b55c9da3481ec22c4729744024aeab15f9b9
chenyi <71563698@qq.com>
2023-01-11 22:41:21 +0800

    备注类型

--- src/main/java/com/schedule/entity/Preference.java
+++ src/main/java/com/schedule/entity/Preference.java
@@ -13,6 +13,12 @@ public class Preference {
 
     private Long id;
 
+    /**
+     * 三种类型：
+     * 工作日偏好  1,3,4
+     * 工作时间偏好  08:00-12:00,18:00-22:00
+     * 班次时长偏好
+     */
     private String preferenceType;
 
     private Long staffId;
