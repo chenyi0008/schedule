c2dd1dfa8c97983d15b0f5a002b27019f5fc37a7
akuya <1828504053@qq.com>
2023-01-17 23:40:10 +0800

    1/14 16:05 akuya modify

--- src/main/java/com/schedule/util/GeneratorUtil.java
+++ src/main/java/com/schedule/util/GeneratorUtil.java
@@ -1,9 +1,11 @@
 package com.schedule.util;
 
+import com.schedule.entity.Flow;
 import org.springframework.util.StringUtils;
 
-import java.util.Arrays;
-import java.util.Random;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.DoubleStream;
 
@@ -26,6 +28,46 @@ public class GeneratorUtil {
         }
         return Arrays.stream(value).mapToObj(String::valueOf).collect(Collectors.joining(","));
     }
+
+    public static List<Flow> RandomGeneration(String datetime,int Cdays,Long storeId){
+
+        SimpleDateFormat f=new SimpleDateFormat("yyyy-MM-dd");
+        Calendar cal=Calendar.getInstance();
+        Date datet=null;
+        try{
+            datet=f.parse(datetime);
+            cal.setTime(datet);
+        }catch (ParseException e){
+            e.printStackTrace();
+        }
+
+        int week=cal.get(Calendar.DAY_OF_WEEK)-1;
+        if(week<0)week=0;
+
+        List<Flow> list=new ArrayList<>();
+
+        for(int i=0;i<Cdays;i++){
+            Flow flow=new Flow();
+            flow.setStoreId(storeId);
+            String temp;
+            if(week==0||week==7){  //{天，一，二，三，四，五，六}
+                 temp=generator(false);
+            }else{
+                 temp=generator(true);
+            }
+            flow.setValue(temp);
+            flow.setDate(datetime);
+            Date tdate=cal.getTime();
+            String ss=tdate.toString();
+            flow.setDate(ss);
+            list.add(flow);
+            week=(week+1)%7;
+            cal.add(Calendar.DAY_OF_MONTH,1);
+        }
+
+        return list;
+    }
+
 
 
     public static void main(String[] args) {
